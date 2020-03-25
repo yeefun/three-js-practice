27825a21dd40651c9a5d66ce52a4bae21027ff56
yeefun <szyeefun@gmail.com>
2020-03-25 16:16:19 +0800

    feat: add frameArea

--- practices/others/prison/js/index.js
+++ practices/others/prison/js/index.js
@@ -32,6 +32,7 @@ function init () {
 
   // camera
   camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 400)
+  // console.log(THREE.Math.degToRad(camera.fov), camera.fov)
   camera.position.set(20, -20, 50)
   camera.lookAt(scene.position)
 
@@ -49,21 +50,31 @@ function init () {
   // loader
   const loader = new GLTFLoader()
   loader.load(`${pathname}/assets/models/gltf/prison-door.glb`, function (gltf) {
-    console.log(gltf)
+    // console.log(gltf)
     const model = gltf.scene
-    // model.position.set(0, 0, 0)
-    model.scale.set(0.01, 0.01, 0.01)
+    // model.scale.set(0.01, 0.01, 0.01)
     // camera = gltf.cameras[0]
-    camera.updateProjectionMatrix()
-    mixer = new THREE.AnimationMixer(model)
-    gltf.animations.forEach((clip) => {
-      mixer.clipAction(clip).play()
-    })
+    // camera.updateProjectionMatrix()
+    // mixer = new THREE.AnimationMixer(model)
+    // gltf.animations.forEach((clip) => {
+    //   mixer.clipAction(clip).play()
+    // })
     // model.traverse((child) => {
     //   if (child.material) child.material.metalness = 0
     // })
 
     scene.add(model)
+    const box = new THREE.Box3().setFromObject(model)
+    console.log(box);
+    
+    const boxSize = box.getSize(new THREE.Vector3()).length()
+    const boxCenter = box.getCenter(new THREE.Vector3())
+    console.log(box.getSize(new THREE.Vector3()));
+    
+    // console.log(boxSize)
+    // console.log(boxCenter)
+    frameArea(boxSize, boxSize, boxCenter, camera)
+    // console.log(dumpObject(model).join('\n'))
     animate()
   })
 
@@ -93,7 +104,7 @@ function init () {
 
 function animate() {
   const delta = clock.getDelta()
-  mixer.update(delta)
+  // mixer.update(delta)
   stats.update()
   renderer.render(scene, camera)
   requestAnimationFrame(animate)
@@ -103,4 +114,41 @@ function handleWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function frameArea (sizeToFitOnScreen, boxSize, boxCenter, camera) {
+  const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5
+  const halfFovY = THREE.Math.degToRad(camera.fov * 0.5)
+  const distance = halfSizeToFitOnScreen / Math.tan(halfFovY)
+
+  // compute a unit vector that points in the direction the camera is now from the center of the box
+  const direction = new THREE.Vector3()
+    .subVectors(camera.position, boxCenter)
+    // in the xz plane
+    // .multiply(new THREE.Vector3(1, 0, 1))
+    .normalize()
+
+  // move the camera to a position distance units way from the center in whatever direction the camera was from the center already  
+  camera.position.copy(direction.multiplyScalar(distance).add(boxCenter))
+
+  // pick some near and far values for the frustum that will contain the box.
+  camera.near = boxSize / 100
+  camera.far = boxSize * 100
+
+  camera.updateProjectionMatrix()
+
+  // point the camera to look at the center of the box
+  camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z)
+}
+
+function dumpObject (obj, lines = [], isLast = true, prefix = '') {
+  const localPrefix = isLast ? '└─' : '├─'
+  lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`)
+  const newPrefix = prefix + (isLast ? '  ' : '│ ')
+  const lastNdx = obj.children.length - 1
+  obj.children.forEach((child, ndx) => {
+    const isLast = (ndx === lastNdx)
+    dumpObject(child, lines, isLast, newPrefix)
+  })
+  return lines
 }
